b95b404514997cd3e6bd980e29e1c78cfafa5255
Gonzalo <gonchogfa@gmail.com>
2024-07-17 10:33:28 +0200

    starting 04/03

---
+++ module04/ex03/AMateria.cpp
@@ -0,0 +1,20 @@
+#include "AMateria.hpp"
+
+AMateria::AMateria() : type("undefined"){}
+
+AMateria::AMateria(std::string const & type) : type(type){}
+
+AMateria::AMateria(const AMateria& original) : type(original.getType()){}
+
+AMateria& AMateria::operator=(const AMateria& original){
+    if (this != &original){
+        type = original.getType();
+    }
+    return *this;
+}
+
+AMateria::~AMateria(){}
+
+std::string const & AMateria::getType() const{return type;}
+
+void AMateria::use(ICharacter& target){}
---
+++ module04/ex03/AMateria.hpp
@@ -0,0 +1,24 @@
+#ifndef AMATERIA_HPP
+#define AMATERIA_HPP
+
+#include <string.h>
+#include <iostream>
+#include "ICharacter.hpp"
+
+class AMateria
+{
+    protected:
+        std::string type;
+    public:
+        AMateria();
+        AMateria(std::string const & type);
+        AMateria(const AMateria& copy);
+        AMateria& operator=(const AMateria& original);
+        ~AMateria();
+
+        std::string const & getType() const; //Returns the materia type
+        virtual AMateria* clone() const = 0;
+        virtual void use(ICharacter& target);
+};
+
+#endif
