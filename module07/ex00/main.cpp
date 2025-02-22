82f460363ac02566cccad46c9648483d16e94341
Gonzalo <gonchogfa@gmail.com>
2025-02-22 12:02:36 +0100

    cc with objs

--- module07/ex00/main.cpp
+++ module07/ex00/main.cpp
@@ -3,12 +3,22 @@
 int main( void ) {
     int a = 2;
     int b = 3;
+
+    std::cout << "a = " << a << ", b = " << b << std::endl << std::endl;
+
+    std::cout << "--- Swapping a and b ---" << std::endl;
     ::swap( a, b );
     std::cout << "a = " << a << ", b = " << b << std::endl;
     std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
     std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
-    std::string c = "chaine1";
-    std::string d = "chaine2";
+    std::cout << std::endl;
+
+    std::string c = "Lucas";
+    std::string d = "Alberto";
+
+    std::cout << "c = " << c << ", d = " << d << std::endl << std::endl;
+
+    std::cout << "--- Swapping c and d ---" << std::endl;
     ::swap(c, d);
     std::cout << "c = " << c << ", d = " << d << std::endl;
     std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
