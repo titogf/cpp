c0ae8d7137d0b496ba57a59c50470348705f60e3
Gonzalo <gonchogfa@gmail.com>
2025-02-22 14:03:35 +0100

    test main 07/02

--- module07/ex02/main.cpp
+++ module07/ex02/main.cpp
@@ -1,18 +1,27 @@
 #include "Array.hpp"
 
 int main(void){
-    Array<int> i;
-
     Array<int> a (4);
     for (int j = 0; j < 4; j++){
-        a[j] = j + 1;
+        try {
+            a[j] = j + 1;
+        } catch (std::out_of_range &e) {
+            std::cout << e.what() << std::endl;
+        }
     }
-    i = a;
 
-    Array<int> b(i);
-    //b.setArr(i);
+    Array<int> i = a;
+    std::cout << "i[3] = " << i[3] << " a[3] = " << a[3] << std::endl;
+
+    //Array<int> b(i);
+    Array<int> b;
+    b.setArr(i);
     for (int j = 0; j < 4; j++){
-        std::cout << b[j] << std::endl;
+        try {
+            std::cout << b[j] << std::endl;
+        } catch (std::out_of_range &e) {
+            std::cout << e.what() << std::endl;
+        }
     }
     std::cout << "Size: " << b.size() << std::endl;
     Array<int> k (32);
